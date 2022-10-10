9ca265952d14b6bd2229a5e2b2c1224d404e103e
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-10 20:49:01 -0300

    Implementação dos método para salvar um userModel e retornar a lista de um UserModel

--- src/main/java/br/com/maiconcardoso/unittest/services/UserService.java
+++ src/main/java/br/com/maiconcardoso/unittest/services/UserService.java
@@ -1,9 +1,29 @@
 package br.com.maiconcardoso.unittest.services;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import br.com.maiconcardoso.unittest.dtos.UserDto;
+import br.com.maiconcardoso.unittest.model.UserModel;
+import br.com.maiconcardoso.unittest.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
     
-    
+    private final UserRepository repository;
+
+    public UserModel saveUser(UserDto userDto) {
+        var userToBeSaved = new UserModel();
+        BeanUtils.copyProperties(userDto, userToBeSaved);
+        return repository.save(userToBeSaved);
+    }
+
+    public List<UserModel> findAllUserModel() {
+        return repository.findAll();
+    }
+
 }
