2a30b3cef0a0b4cb1b722c1739820ec28bd95abe
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-10 20:48:03 -0300

    User model renomeado

--- src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
+++ src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
@@ -3,9 +3,9 @@ package br.com.maiconcardoso.unittest.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.maiconcardoso.unittest.model.User;
+import br.com.maiconcardoso.unittest.model.UserModel;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<UserModel, Integer>{
     
 }
