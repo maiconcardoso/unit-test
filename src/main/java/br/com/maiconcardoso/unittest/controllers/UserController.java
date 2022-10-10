0a4da91f8b99d76ff9a1a67641a2ea1022349296
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-10 20:46:05 -0300

    Criação do controlador para userModel

--- src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
+++ src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
@@ -1,10 +1,38 @@
 package br.com.maiconcardoso.unittest.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.maiconcardoso.unittest.dtos.UserDto;
+import br.com.maiconcardoso.unittest.model.UserModel;
+import br.com.maiconcardoso.unittest.services.UserService;
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping(name = "api/user")
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<List<UserModel>> findAllUserModel() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAllUserModel());
+    }  
+
+    @PostMapping
+    public ResponseEntity<Void> saveUserModel(@RequestBody @Valid UserDto userDto) {
+        userService.saveUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
     
 }
