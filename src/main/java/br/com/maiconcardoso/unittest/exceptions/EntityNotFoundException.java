a655af7c2c2f8b0d2060d6d2674b99f686f24a61
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-11 06:35:01 -0300

    Modifição do método acessor, de private para public

--- src/main/java/br/com/maiconcardoso/unittest/exceptions/EntityNotFoundException.java
+++ src/main/java/br/com/maiconcardoso/unittest/exceptions/EntityNotFoundException.java
@@ -2,7 +2,7 @@ package br.com.maiconcardoso.unittest.exceptions;
 
 public class EntityNotFoundException extends RuntimeException{
 
-    private static final String MESSAGE = "Entity Not Found";
+    public static final String MESSAGE = "Entity Not Found";
      
     public EntityNotFoundException(String msg) {
         super(msg);
