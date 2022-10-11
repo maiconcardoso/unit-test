61e2ca66b81e5b09d25ba044f21964d786e01b00
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-11 06:34:24 -0300

    Criação do método findByIdUserModel

--- src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
+++ src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/api/user")
+@RequestMapping(path = "/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -29,10 +30,16 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findAllUserModel());
     }  
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> findByIdUserModel(@PathVariable Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findByIdUserModel(id));
+    }
+
     @PostMapping
     public ResponseEntity<Void> saveUserModel(@RequestBody @Valid UserDto userDto) {
         userService.saveUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+    
     
 }
