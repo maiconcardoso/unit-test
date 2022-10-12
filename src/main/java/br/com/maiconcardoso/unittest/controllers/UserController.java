3a65878e874bb8f7cd1806a45b4c5b76f65f453f
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-12 20:34:32 -0300

    Inserção dos métodos de atualização, deleção e retorno de lista de usuários através do nome

--- src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
+++ src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
@@ -6,12 +6,16 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientResponseException;
 
 import br.com.maiconcardoso.unittest.dtos.UserDto;
 import br.com.maiconcardoso.unittest.model.UserModel;
@@ -35,11 +39,29 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findByIdUserModel(id));
     }
 
+    @GetMapping
+    public ResponseEntity<List<UserModel>> findByNameUserModel(@RequestParam(required = false) String name ) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findByNameUser(name));
+    }
+
     @PostMapping
     public ResponseEntity<Void> saveUserModel(@RequestBody @Valid UserDto userDto) {
         userService.saveUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserModel> updateUserModel(@PathVariable Integer id, @RequestBody UserDto UserDto) {
+        return new ResponseEntity<UserModel>(userService.updateUserModel(id, UserDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserModel(@PathVariable Integer id) {
+        userService.deleteUserModel(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
     
     
 }
