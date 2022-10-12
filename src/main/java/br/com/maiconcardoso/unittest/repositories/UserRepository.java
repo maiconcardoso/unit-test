e29824309d0bcdeef837dd1d68842de87ca54a39
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-12 20:20:45 -0300

    Modificação do método e inserção de query

--- src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
+++ src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package br.com.maiconcardoso.unittest.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.maiconcardoso.unittest.model.UserModel;
@@ -10,6 +11,7 @@ import br.com.maiconcardoso.unittest.model.UserModel;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer>{
 
-    Optional<UserModel> findByUserName(String name);
+    @Query(" SELECT obj FROM UserModel obj WHERE obj.name LIKE :name ")
+    Optional<UserModel> findByNameUser(String name);
     
 }
