8ea5126a324c358d4c968c152794e408ed03631b
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-12 20:35:04 -0300

    Modifição do método para retornar uma lista de usuários

--- src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
+++ src/main/java/br/com/maiconcardoso/unittest/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package br.com.maiconcardoso.unittest.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,6 @@ import br.com.maiconcardoso.unittest.model.UserModel;
 public interface UserRepository extends JpaRepository<UserModel, Integer>{
 
     @Query(" SELECT obj FROM UserModel obj WHERE obj.name LIKE :name ")
-    Optional<UserModel> findByNameUser(String name);
+    Optional<List<UserModel>> findByNameUser(String name);
     
 }
