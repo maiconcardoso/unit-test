ae232324050e0dddf5dccd6dee0e33c9cf244a49
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-12 23:41:19 -0300

    Removendo imports não utilizados

--- src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
+++ src/main/java/br/com/maiconcardoso/unittest/controllers/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientResponseException;
 
 import br.com.maiconcardoso.unittest.dtos.UserDto;
 import br.com.maiconcardoso.unittest.model.UserModel;
