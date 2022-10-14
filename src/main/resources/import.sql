072e789becf4f75ecea643488b893a1d530c9d72
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-13 21:24:36 -0300

    Inserido novo usuário através do comando sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO TB_USER (email, name, password) VALUES ('maicon@hotmail.com', 'maicon', '123')
+INSERT INTO TB_USER (email, name, password) VALUES ('maicon@hotmail.com', 'maicon', '123');
+INSERT INTO TB_USER (email, name, password) VALUES ('maria@gmail.com', 'maria', '321');
