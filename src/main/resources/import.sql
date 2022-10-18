52c4a4284afa9c3d2fc703e72e24effc7e6ba369
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-17 22:21:30 -0300

    Retirado dados para não interferir no teste de banco de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO TB_USER (email, name, password) VALUES ('maicon@hotmail.com', 'maicon', '123');
-INSERT INTO TB_USER (email, name, password) VALUES ('maria@gmail.com', 'maria', '321');
+-- INSERT INTO TB_USER (email, name, password) VALUES ('maicon@hotmail.com', 'maicon', '123');
+-- INSERT INTO TB_USER (email, name, password) VALUES ('maria@gmail.com', 'maria', '321');
