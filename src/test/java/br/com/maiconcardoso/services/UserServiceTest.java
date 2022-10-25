931bf3dcd7c90c25dd2bf60d51ad7f56fe53346c
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-25 07:04:53 -0300

    Desenvolvimento dos metódos de teste busca por id, lançamento de exceção e retorno de lista de usuários

--- src/test/java/br/com/maiconcardoso/services/UserServiceTest.java
+++ src/test/java/br/com/maiconcardoso/services/UserServiceTest.java
@@ -3,16 +3,18 @@ package br.com.maiconcardoso.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.maiconcardoso.unittest.exceptions.EntityNotFoundException;
 import br.com.maiconcardoso.unittest.model.UserModel;
 import br.com.maiconcardoso.unittest.repositories.UserRepository;
 import br.com.maiconcardoso.unittest.services.UserService;
@@ -30,10 +32,10 @@ public class UserServiceTest {
     @BeforeEach
     public void setUp() {
 
-        PageImpl<UserModel> userPage = new PageImpl<>(List.of(UserCreator.createUserToBeSaved()));
+        // PageImpl<UserModel> userPage = new PageImpl<>(List.of(UserCreator.createUserToBeSaved()));
 
-        BDDMockito.when(this.userRepositoryMock.findAll(ArgumentMatchers.any(PageRequest.class)))
-            .thenReturn(userPage);
+        // BDDMockito.when(this.userRepositoryMock.findAll(ArgumentMatchers.any(PageRequest.class)))
+        //     .thenReturn(userPage); // Resultado Paginado
 
         BDDMockito.when(this.userRepositoryMock.findAll())
             .thenReturn(List.of(UserCreator.createUserToBeSaved()));
@@ -46,5 +48,41 @@ public class UserServiceTest {
 
         BDDMockito.doNothing().when(this.userRepositoryMock).delete(ArgumentMatchers.any());
 
+    }
+
+    @Test
+    @DisplayName("FindAll returns of user list when succesful")
+    public void findAll_ReturnsUserList_WhenSuccessful() {
+        String expectedName = UserCreator.createUserToBeSaved().getName();
+
+        List<UserModel> userList = userService.findAllUserModel();
+
+        Assertions.assertThat(userList).isNotEmpty().isNotNull().hasSize(1);
+
+        Assertions.assertThat(userList.get(0).getName()).isEqualTo(expectedName);
+    }
+    
+    @Test
+    @DisplayName("findByIdUserModel return user when succesful")
+    public void findByIdUserModel_ReturnUser_WhenSuccessful() {
+        UserModel expectedId = UserCreator.createUserToBeSaved();
+
+        UserModel userById = userService.findByIdUserModel(expectedId.getId());
+
+        Assertions.assertThat(userById).isNotNull();
+
+        Assertions.assertThat(expectedId.getId()).isEqualTo(userById.getId());
+    }
+
+    @Test
+    @DisplayName("findByIdUserModel throw Exception when user is not found")
+    public void findByIdUserModel_ThrowException_WhenUserIsNotFound() {
+        BDDMockito.when(this.userRepositoryMock.findById(ArgumentMatchers.anyInt()))
+            .thenReturn(Optional.empty());
+
+        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+            .isThrownBy(() -> userService.findByIdUserModel(1));
+
+        
     }
 }
