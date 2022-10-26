c70e60293cf471264d5d44a5db197c01a0717424
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-25 21:47:13 -0300

    Alteração do construtor

--- src/main/java/br/com/maiconcardoso/unittest/dtos/UserDto.java
+++ src/main/java/br/com/maiconcardoso/unittest/dtos/UserDto.java
@@ -1,5 +1,6 @@
 package br.com.maiconcardoso.unittest.dtos;
 
+import br.com.maiconcardoso.unittest.model.UserModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,15 @@ public class UserDto {
     private String name;
     private String email;
     private String password;
+
+    public UserDto(UserModel userModel) {
+        this.id = userModel.getId();
+        this.name = userModel.getName();
+        this.email = userModel.getEmail();
+        this.password = userModel.getPassword();
+    }
+
+
+
+
 }
