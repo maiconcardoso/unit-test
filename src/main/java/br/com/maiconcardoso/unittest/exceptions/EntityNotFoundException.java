383194a65b36ef5f9d704850ad9cc31f5db306c5
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-28 07:19:20 -0300

    Inserção da anotação ResponseStatus

--- src/main/java/br/com/maiconcardoso/unittest/exceptions/EntityNotFoundException.java
+++ src/main/java/br/com/maiconcardoso/unittest/exceptions/EntityNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.maiconcardoso.unittest.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException{
 
     public static final String MESSAGE = "Entity Not Found";
